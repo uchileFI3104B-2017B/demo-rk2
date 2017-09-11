6b0f34bc3e858ecb8aac0889a1262847f83d5231
Valentino Gonzalez <valentino.g.gonzalez@gmail.com>
2017-09-11 12:38:40 -0300

    grafica solucion de pequeñas oscilaciones

--- rk2.py
+++ rk2.py
@@ -3,3 +3,23 @@ Integra la ecuacion del pendulo usando RK2.
 
 Usaremos l = 1.
 """
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+g = 9.8
+l = 1.0
+
+# Condiciones de borde
+phi_0 = np.pi / 8
+w_0 = 0
+
+
+T = 2 * np.pi / np.sqrt(g/l)
+t = np.linspace(0, 4 * T, 300)
+phi_pequenas_oscilaciones = phi_0 * np.cos(np.sqrt(g/l) * t)
+
+
+plt.clf()
+plt.plot(t, phi_pequenas_oscilaciones, label="pequenas oscilaciones")
+plt.show()
