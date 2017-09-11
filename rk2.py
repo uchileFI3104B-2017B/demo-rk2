6e9924a93ed78d4cdfa804c5cf0fe7095be19c2a
Valentino Gonzalez <valentino.g.gonzalez@gmail.com>
2017-09-11 13:25:07 -0300

    modifica encoding a utf-8

--- rk2.py
+++ rk2.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 """
 Integra la ecuacion del pendulo usando RK2.
 
@@ -19,9 +21,8 @@ T = 2 * np.pi / np.sqrt(g/l)
 t = np.linspace(0, 4 * T, 300)
 phi_pequenas_oscilaciones = phi_0 * np.cos(np.sqrt(g/l) * t)
 
-
 plt.clf()
-plt.plot(t, phi_pequenas_oscilaciones, label="pequenas oscilaciones")
+plt.plot(t, phi_pequenas_oscilaciones, label=u"pequeñas oscilaciones")
 
 def f_pendulo(t, y):
     phi, w = y
